ef55cd7a21466bc83ce40b71a506a45c278ac98e
djangogo <xizero00@gmail.com>
2019-03-27 23:19:01 +0800

    add clean script

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Delete all pyc python bytecode file
+find -name "*.pyc" -not -path "./venv/*" -delete
+# find . -name '*.pyc' -delete
+# find . -name "*.pyc" -exec rm -f {} \;
+
+# Delete all objs in libs folder
+pushd libs
+find . -name '*.o' -delete
+find . -name '*.cu' -delete
+popd
